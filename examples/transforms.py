c0764d1b80f508bb1685da44f7c6653b141f85a9
Mikael Mortensen <mikaem@math.uio.no>
2017-05-09 10:08:19 +0200

    Change name of transforms_realdata + testing for slab

--- examples/transforms.py
+++ examples/transforms.py
@@ -6,10 +6,10 @@ from time import time
 
 # Set global size of the computational box
 M = 4
-N = array([13, 13, 13], dtype=int)
+N = array([25, 28, 23], dtype=int)
 
-fft = PFFT(MPI.COMM_WORLD, N, axes=(0,1,2), collapse=False)
-pfft = PFFT(MPI.COMM_WORLD, N, axes=(0,1,2), padding=[1.5, 1.5, 1.5])
+fft = PFFT(MPI.COMM_WORLD, N, axes=(0,1,2), collapse=False, slab=True)
+pfft = PFFT(MPI.COMM_WORLD, N, axes=(0,1,2), padding=[1.5, 1.5, 1.5], slab=True)
 
 u = Function(fft, False)
 u[:] = random.random(u.shape).astype(u.dtype)
@@ -19,9 +19,6 @@ u_hat = fft.forward(u, u_hat)
 uj = zeros_like(u)
 uj = fft.backward(u_hat, uj)
 assert allclose(uj, u)
-
-#print("U", u.shape)
-#print("U_hat", u_hat.shape)
 
 u_padded = zeros(pfft.forward.input_array.shape)
 uc = u_hat.copy()
@@ -37,19 +34,5 @@ u3 = uc.copy()
 u3 = cfft.forward(u2, u3)
 
 assert allclose(uc, u3)
-
-M = 4
-N = array([31, 32], dtype=int)
-
-fft = PFFT(MPI.COMM_WORLD, N, axes=(0,1))
-u = Function(fft, False)
-u_hat = Function(fft)
-print("U0", u.shape)
-print("U0_hat", u_hat.shape)
-fft = PFFT(MPI.COMM_WORLD, N, axes=(1,0))
-u = Function(fft, False)
-u_hat = Function(fft)
-print("U", u.shape)
-print("U_hat", u_hat.shape)
 
 
--- tests/test_mpifft.py
+++ tests/test_mpifft.py
@@ -32,106 +32,106 @@ def test_mpifft():
                         n //=2; n+=1
                     if n < comm.size:
                         continue
+                for slab in (True, False):
+                    padding = False
+                    for axes in [None, (-1,), (-2,),
+                                (-1,-2,), (-2,-1),
+                                (-1,0), (0,-1)]:
 
-                padding = False
-                for axes in [None, (-1,), (-2,),
-                             (-1,-2,), (-2,-1),
-                             (-1,0), (0,-1)]:
+                        fft = PFFT(comm, shape, axes=axes, dtype=typecode,
+                                padding=padding, slab=slab)
 
-                    fft = PFFT(comm, shape, axes=axes, dtype=typecode,
-                               padding=padding)
+                        if comm.rank == 0:
+                            grid = [c.size for c in fft.subcomm]
+                            print('grid:{} shape:{} typecode:{} axes:{}'
+                                .format(grid, shape, typecode, axes,))
 
-                    if comm.rank == 0:
-                        grid = [c.size for c in fft.subcomm]
-                        print('grid:{} shape:{} typecode:{} axes:{}'
-                              .format(grid, shape, typecode, axes,))
+                        assert len(fft.axes) == len(fft.xfftn)
+                        assert len(fft.axes) == len(fft.transfer) + 1
+                        #assert fft.axes[-1] == fft.forward._xfftn[0].axes
+                        #assert fft.axes[-1] == fft.backward._xfftn[-1].axes
+                        #assert fft.axes[0] == fft.forward._xfftn[-1].axes
+                        #assert fft.axes[0] == fft.backward._xfftn[0].axes
+                        assert (fft.forward.input_pencil.subshape ==
+                                fft.forward.input_array.shape)
+                        assert (fft.forward.output_pencil.subshape ==
+                                fft.forward.output_array.shape)
+                        assert (fft.backward.input_pencil.subshape ==
+                                fft.backward.input_array.shape)
+                        assert (fft.backward.output_pencil.subshape ==
+                                fft.backward.output_array.shape)
+                        ax = -1 if axes is None else axes[-1]
+                        assert fft.forward.input_pencil.substart[ax] == 0
+                        assert fft.backward.output_pencil.substart[ax] == 0
+                        ax = 0 if axes is None else axes[0]
+                        assert fft.forward.output_pencil.substart[ax] == 0
+                        assert fft.backward.input_pencil.substart[ax] == 0
 
-                    assert len(fft.axes) == len(fft.xfftn)
-                    assert len(fft.axes) == len(fft.transfer) + 1
-                    #assert fft.axes[-1] == fft.forward._xfftn[0].axes
-                    #assert fft.axes[-1] == fft.backward._xfftn[-1].axes
-                    #assert fft.axes[0] == fft.forward._xfftn[-1].axes
-                    #assert fft.axes[0] == fft.backward._xfftn[0].axes
-                    assert (fft.forward.input_pencil.subshape ==
-                            fft.forward.input_array.shape)
-                    assert (fft.forward.output_pencil.subshape ==
-                            fft.forward.output_array.shape)
-                    assert (fft.backward.input_pencil.subshape ==
-                            fft.backward.input_array.shape)
-                    assert (fft.backward.output_pencil.subshape ==
-                            fft.backward.output_array.shape)
-                    ax = -1 if axes is None else axes[-1]
-                    assert fft.forward.input_pencil.substart[ax] == 0
-                    assert fft.backward.output_pencil.substart[ax] == 0
-                    ax = 0 if axes is None else axes[0]
-                    assert fft.forward.output_pencil.substart[ax] == 0
-                    assert fft.backward.input_pencil.substart[ax] == 0
+                        U = random_like(fft.forward.input_array)
 
-                    U = random_like(fft.forward.input_array)
+                        if 1:
+                            F = fft.forward(U)
+                            V = fft.backward(F)
+                            assert allclose(V, U)
+                        else:
+                            fft.forward.input_array[...] = U
+                            fft.forward()
+                            fft.backward()
+                            V = fft.backward.output_array
+                            assert allclose(V, U)
 
-                    if 1:
+                        fft.destroy()
+
+                    padding = [1.5]*len(shape)
+                    for axes in [None, (-1,), (-2,),
+                                (-1,-2,), (-2,-1),
+                                (-1,0), (0,-1)]:
+
+                        fft = PFFT(comm, shape, axes=axes, dtype=typecode,
+                                padding=padding, slab=slab)
+
+                        if comm.rank == 0:
+                            grid = [c.size for c in fft.subcomm]
+                            print('grid:{} shape:{} typecode:{} axes:{}'
+                                .format(grid, shape, typecode, axes,))
+
+                        assert len(fft.axes) == len(fft.xfftn)
+                        assert len(fft.axes) == len(fft.transfer) + 1
+                        #assert fft.axes[-1] == fft.forward._xfftn[0].axes
+                        #assert fft.axes[-1] == fft.backward._xfftn[-1].axes
+                        #assert fft.axes[0] == fft.forward._xfftn[-1].axes
+                        #assert fft.axes[0] == fft.backward._xfftn[0].axes
+                        assert (fft.forward.input_pencil.subshape ==
+                                fft.forward.input_array.shape)
+                        assert (fft.forward.output_pencil.subshape ==
+                                fft.forward.output_array.shape)
+                        assert (fft.backward.input_pencil.subshape ==
+                                fft.backward.input_array.shape)
+                        assert (fft.backward.output_pencil.subshape ==
+                                fft.backward.output_array.shape)
+                        ax = -1 if axes is None else axes[-1]
+                        assert fft.forward.input_pencil.substart[ax] == 0
+                        assert fft.backward.output_pencil.substart[ax] == 0
+                        ax = 0 if axes is None else axes[0]
+                        assert fft.forward.output_pencil.substart[ax] == 0
+                        assert fft.backward.input_pencil.substart[ax] == 0
+
+                        U = random_like(fft.forward.input_array)
                         F = fft.forward(U)
-                        V = fft.backward(F)
-                        assert allclose(V, U)
-                    else:
-                        fft.forward.input_array[...] = U
-                        fft.forward()
-                        fft.backward()
-                        V = fft.backward.output_array
-                        assert allclose(V, U)
 
-                    fft.destroy()
+                        if 1:
+                            Fc = F.copy()
+                            V = fft.backward(F)
+                            F = fft.forward(V)
+                            assert allclose(F, Fc)
+                        else:
+                            fft.backward.input_array[...] = F
+                            fft.backward()
+                            fft.forward()
+                            V = fft.forward.output_array
+                            assert allclose(F, V)
 
-                padding = [1.5]*len(shape)
-                for axes in [None, (-1,), (-2,),
-                             (-1,-2,), (-2,-1),
-                             (-1,0), (0,-1)]:
-
-                    fft = PFFT(comm, shape, axes=axes, dtype=typecode,
-                               padding=padding)
-
-                    if comm.rank == 0:
-                        grid = [c.size for c in fft.subcomm]
-                        print('grid:{} shape:{} typecode:{} axes:{}'
-                              .format(grid, shape, typecode, axes,))
-
-                    assert len(fft.axes) == len(fft.xfftn)
-                    assert len(fft.axes) == len(fft.transfer) + 1
-                    #assert fft.axes[-1] == fft.forward._xfftn[0].axes
-                    #assert fft.axes[-1] == fft.backward._xfftn[-1].axes
-                    #assert fft.axes[0] == fft.forward._xfftn[-1].axes
-                    #assert fft.axes[0] == fft.backward._xfftn[0].axes
-                    assert (fft.forward.input_pencil.subshape ==
-                            fft.forward.input_array.shape)
-                    assert (fft.forward.output_pencil.subshape ==
-                            fft.forward.output_array.shape)
-                    assert (fft.backward.input_pencil.subshape ==
-                            fft.backward.input_array.shape)
-                    assert (fft.backward.output_pencil.subshape ==
-                            fft.backward.output_array.shape)
-                    ax = -1 if axes is None else axes[-1]
-                    assert fft.forward.input_pencil.substart[ax] == 0
-                    assert fft.backward.output_pencil.substart[ax] == 0
-                    ax = 0 if axes is None else axes[0]
-                    assert fft.forward.output_pencil.substart[ax] == 0
-                    assert fft.backward.input_pencil.substart[ax] == 0
-
-                    U = random_like(fft.forward.input_array)
-                    F = fft.forward(U)
-
-                    if 1:
-                        Fc = F.copy()
-                        V = fft.backward(F)
-                        F = fft.forward(V)
-                        assert allclose(F, Fc)
-                    else:
-                        fft.backward.input_array[...] = F
-                        fft.backward()
-                        fft.forward()
-                        V = fft.forward.output_array
-                        assert allclose(F, V)
-
-                    fft.destroy()
+                        fft.destroy()
 
 if __name__ == '__main__':
     test_mpifft()
