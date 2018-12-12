f86c42d2274dcd4ab3260a7aa5888be26af2c2e2
Mikael Mortensen <mikaem@math.uio.no>
2018-12-12 10:42:03 +0100

    Bump version

--- mpi4py_fft/__init__.py
+++ mpi4py_fft/__init__.py
@@ -9,14 +9,14 @@ Fourier Transforms (FFTs) of (large) multidimensional arrays. There is really
 no limit as to how large the arrays can be, just as long as there is sufficient
 computing powers available. Also, there are no limits as to how transforms can
 be configured. Just about any combination of transforms from the FFTW library
-is supported. Furthermore, mpi4py-fft can also be used simply to perform global
-redistributions (distribute and communicate) of large arrays with MPI, without
-any transforms at all.
+is supported. Furthermore, **mpi4py-fft** can also be used simply to perform
+global redistributions (distribute and communicate) of large arrays with MPI,
+without any transforms at all.
 
 For more information, see `documentation <https://mpi4py-fft.readthedocs.io>`_.
 
 """
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 __author__ = 'Lisandro Dalcin and Mikael Mortensen'
 
 from .mpifft import PFFT, Function
