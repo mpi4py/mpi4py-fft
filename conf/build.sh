bc549e8dd5de284815811d2cf90fb90b553d4435
Mikael Mortensen <mikaem@math.uio.no>
2019-01-28 13:22:36 +0100

    Fixing circleci config

--- conf/build.sh
+++ conf/build.sh
@@ -6,4 +6,7 @@ then
     export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 
+export CFLAGS=$(echo $CFLAGS | sed -E 's@\-fdebug\-prefix\-map[^ ]*@@g')
+export CXXFLAGS=$(echo $CXXFLAGS | sed -E 's@\-fdebug\-prefix\-map[^ ]*@@g')
+
 $PYTHON setup.py install --prefix=$PREFIX
