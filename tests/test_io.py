d4769398d623962ba67d5b09d9a1eaa9fe8a30d9
Mikael Mortensen <mikaem@math.uio.no>
2019-03-15 08:55:45 +0100

    Fixing coverage

--- tests/test_io.py
+++ tests/test_io.py
@@ -56,6 +56,9 @@ def test_2D(backend, forward_output):
             u0.read(filename, 'u', 2)
             u0.read(read, 'u', 2)
             assert np.allclose(u0, u)
+            if backend == 'netcdf4': # Test opening file in mode 'a' when not existing
+                remove_if_exists('nctesta.nc')
+                ncfile = NCFile('nctesta.nc', domain=domain, mode='a')
     T.destroy()
 
 def test_3D(backend, forward_output):
