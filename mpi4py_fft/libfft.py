026dea410e6c856da0a88bccf89d5ecd9eaec82d
Mikael Mortensen <mikaem@math.uio.no>
2019-04-08 17:11:37 +0200

    Fixing more py27 stuff

--- mpi4py_fft/libfft.py
+++ mpi4py_fft/libfft.py
@@ -41,7 +41,6 @@ def _Xfftn_plan_pyfftw(shape, axes, dtype, transforms, options):
     functools.update_wrapper(wrapped_xfftn_bck, xfftn_bck,
                              assigned=['input_array',
                                        'output_array',
-                                       '__name__',
                                        '__doc__',
                                        '__module__'])
 
