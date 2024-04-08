a29121d46bb80fca005518e1266d2e479d77a4b3
Ben Landrum <71329856+benlandrum@users.noreply.github.com>
2024-04-07 22:41:14 -0400

    Simplify OS switches in workflow (#6)

--- .github/actions/setup-fftw/setup-fftw.sh
+++ .github/actions/setup-fftw/setup-fftw.sh
@@ -4,44 +4,37 @@
 # ubuntu-latest -> ubuntu
 os=${1%-*}
 
-setup-apt-fftw () {
-    sudo apt update && sudo apt install -y -q libfftw3-dev
-}
-
-setup-brew-fftw () {
+install-package-macos () {
     brew install fftw
 }
 
-setup-env-fftw () {
-    case "$os" in
-	macos)
-	    prefix=$(brew --prefix fftw)
-	    echo "include-dir=$prefix/include" >> "$GITHUB_OUTPUT"
-	    echo "library-dir=$prefix/lib" >> "$GITHUB_OUTPUT"
-	    ;;
-	ubuntu)
-	    echo "include-dir=/usr/include" >> "$GITHUB_OUTPUT"
-	    echo "library-dir=/usr/lib/x86_64-linux-gnu" \
-		 >> "$GITHUB_OUTPUT"
-	    ;;
-	*)
-	    echo os "$os" not recognized
-	    exit 1
-	    ;;
-    esac
+install-package-ubuntu () {
+    sudo apt update && sudo apt install -y -q libfftw3-dev
 }
 
-case $(uname) in
-    Linux)
-	setup-apt-fftw
+setup-env-ubuntu() {
+    echo "include-dir=/usr/include" >> "$GITHUB_OUTPUT"
+    echo "library-dir=/usr/lib/x86_64-linux-gnu" \
+	 >> "$GITHUB_OUTPUT"
+}
+
+setup-env-macos() {
+    local prefix=$(brew --prefix fftw)
+    echo "include-dir=$prefix/include" >> "$GITHUB_OUTPUT"
+    echo "library-dir=$prefix/lib" >> "$GITHUB_OUTPUT"
+}
+
+case "$os" in
+    macos)
+	install-package-macos
+	setup-env-macos
 	;;
-    Darwin)
-	setup-brew-fftw
+    ubuntu)
+	install-package-ubuntu
+	setup-env-ubuntu
 	;;
     *)
-	echo uname $(uname) not recognized
+	echo os "$os" not recognized
 	exit 1
 	;;
 esac
-
-setup-env-fftw
